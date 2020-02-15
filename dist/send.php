0a2aef184647737559edd72e383787a1fc9c011a
Max Levitskiy <gmaxlev@gmail.com>
2020-02-15 09:41:09 +0200

    fix send email

--- dist/send.php
+++ dist/send.php
@@ -24,11 +24,11 @@
 
         $text_message = "
 
-            Новая заявка на сайте.\n
-            Форма: 'Узнать стоимость'\n\n
+            Новая заявка на сайте. <br>
+            Форма: 'Узнать стоимость' <br><br>
 
-            Имя: ".$form_name ." <br>\n
-            Номер телефона: ".$form_phone." <br>\n
+            Имя: ".$form_name ." <br>
+            Номер телефона: ".$form_phone." <br>
             
         ";
 
@@ -47,22 +47,29 @@
 
         $text_message = "
 
-            Новая заявка на сайте.\n
-            Форма: 'Задать вопрос'\n\n
+            Новая заявка на сайте.<br>
+            Форма: 'Задать вопрос'<br><br>
 
-            Имя: ".$form_name ." <br>\n
-            Номер телефона: ".$form_phone." <br>\n
-            Комментарий: ".$form_message." <br>\n
+            Имя: ".$form_name ." <br>
+            Номер телефона: ".$form_phone." <br>
+            Комментарий: ".$form_message." <br>
         ";
 
     }
 
-    $subject = "Новая заявка на сайте!";
-    $header = "Content-type: text/html\r\n";
 
-    $email_to = implode(',', $recipients);
-
-    $retval = mail ($recipients,$subject,$text_message,$header);
+    $mail['charset'] = "utf-8";
+    $mail['to'] = implode(',', $recipients);
+    $mail['subject'] = 'Новое сообщение на сайте';
+    $mail['massage'] = $text_message;
+    
+    $mail['header'] = "MIME-Version: 1.0\n"
+    ."X-Priority: 3\n"
+    ."X-Mailer: Mailer\n"
+    ."Content-Transfer-Encoding: 8bit\n"
+    ."Content-Type: text/html; charset=" . $mail['charset'] . "\n";
+    
+    mail ($mail['to'], $mail['subject'], $mail['massage'], $mail['header']);
 
 
 ?>
